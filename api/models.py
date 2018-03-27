c7d121b113dfcc4a0e78d22e9186b0d6afc2d876
Mohammad <mohammadnassiri>
2018-03-27 14:52:41 +0430

    add vbox model.

--- api/models.py
+++ api/models.py
@@ -13,7 +13,14 @@ class Record(models.Model):
     screen_shot = models.FileField(upload_to="screenshot")
     malware = models.BooleanField(default=False)
     run_pe = models.BooleanField(default=False)
-    status = models.IntegerField(default=0)
+    status = models.IntegerField(default=0)  # 0=waiting, 1=on process, 2=traced, 3=error
     vbox = models.CharField(max_length=255)
     created_time = models.DateTimeField(default=datetime.now)
     updated_time = models.DateTimeField(default=datetime.now)
+
+
+class Vbox(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    status = models.IntegerField(default=0)  # 0=idle, 1=running, 2=complete-idle, 3=error not use
+    time = models.DateTimeField(default=datetime.now)
+
